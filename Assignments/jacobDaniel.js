ce2533064baacea46c1d5ded22a41e90ce1dbd4f
Harith Onigemo <45686269+Harithmetic1@users.noreply.github.com>
2020-10-11 08:10:45 +0100

    Delete jacobDaniel.js

--- Assignments/jacobDaniel.js
+++
@@ -1,5 +0,0 @@
-//Assignment : Fahrenheit temperature converter 
-let celciusTemp = 37 ;
-let converter = (9/5 * celciusTemp) +32;
-const unitFarenheit = '°F';
-console. log(converter + unitFarenheit)
